2391612cf6fd284f7c21a6df63facf08a7c11c67
Max Phillips <maxvphillips@gmail.com>
2022-04-07 20:59:03 -0400

    Added JS to open all links in new tab

--- index.js
+++ index.js
@@ -124,3 +124,20 @@ const element = document.querySelectorAll("span");
 for (let i = 0; i < element.length; i++) {
     element[i].style.transform = "rotate(" + i * 15 + "deg)";
 }
+
+// open all external links in new tab
+(function () {
+    const links = document.querySelectorAll(
+        "a[href^='https://'], a[href^='http://']"
+    );
+    const host = window.location.hostname;
+
+    const isInternalLink = (link) => new URL(link).hostname === host;
+
+    links.forEach((link) => {
+        if (isInternalLink(link)) return;
+
+        link.setAttribute("target", "_blank");
+        link.setAttribute("rel", "noopener noreferrer");
+    });
+})();
